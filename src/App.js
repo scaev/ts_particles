8b02374b1f84ac2d07418549ae4faff760f60fb1
Kaan <kenkaraa@gmail.com>
2023-04-27 18:00:47 -0400

    Adds react-particles & tsparticles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover text-white relative overflow-hidden">
-      <h1>testt</h1>
+      <div>
+        <Design />
+      </div>
     </div>
   );
 }
---
+++ src/components/Design.js
@@ -0,0 +1,7 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+export const Design = () => {
+  return <div>Design</div>;
+};
