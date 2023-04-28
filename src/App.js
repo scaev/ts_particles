dfa0bb0199690beb0902c5f228e323d8affa3875
Kaan <kenkaraa@gmail.com>
2023-04-28 15:34:11 -0400

    Adds header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Design } from "./components/Design";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover text-white relative overflow-hidden">
+            <div className="max-w-6xl mx-auto text-white">
+        <Header />
+      </div>
       <div className="w-full- h-screen absolute top-0 left-0">
         <Design />
       </div>
