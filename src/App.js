439da3f533fadda437f918163ad8e9b1a2c23d25
Kaan <kenkaraa@gmail.com>
2023-04-28 15:50:18 -0400

    Adds Banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Design } from "./components/Design";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover text-white relative overflow-hidden">
             <div className="max-w-6xl mx-auto text-white">
         <Header />
+        <Banner />
       </div>
       <div className="w-full- h-screen absolute top-0 left-0">
         <Design />
